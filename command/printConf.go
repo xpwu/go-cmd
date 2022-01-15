948bd56eab346eea2967d82f7c8b0794e7565b33
xpwu <diodexp@foxmail.com>
2022-01-15 21:56:11 +0800

    修改print命令的具体实现

--- command/printConf.go
+++ command/printConf.go
@@ -6,25 +6,15 @@ import (
   "os"
 )
 
-type printConfig struct {
-
-}
-
-//func NewPrintConfig() command.Command {
-//  return printConfig{}
-//}
-
-func (p printConfig)Run()  {
-  config.Print()
-  os.Exit(0)
-}
-
 func init() {
   argP := false
   tinyFlag.BoolVar(&argP, "p", false, "打印配置项及默认值")
   RegisterCommand(func() bool {
     return argP
   }, func() Command {
-    return printConfig{}
+    return FuncCommand(func() {
+      config.Print()
+      os.Exit(0)
+    })
   }, NotAutoReadConf())
 }
