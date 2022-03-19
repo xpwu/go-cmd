4679edc5c06450bc54acefdc504795bdc90869fa
xpwu <diodexp@foxmail.com>
2022-03-19 15:48:53 +0800

    read config

---
+++ arg/defaultarg.go
@@ -0,0 +1,40 @@
+package arg
+
+import (
+  "github.com/xpwu/go-cmd/exe"
+  "github.com/xpwu/go-config/configs"
+  "path/filepath"
+)
+
+type option struct {
+  name string
+}
+
+type Options func(o *option)
+
+func Name(v string) Options {
+  return func(o *option) {
+    o.name = v
+  }
+}
+
+func ReadConfig(arg *Arg, opts... Options) {
+  opt := &option{
+    name: "c",
+  }
+  for _,o := range opts {
+    o(opt)
+  }
+
+  config := "config.json"
+  arg.String(&config, opt.name, "config file path")
+
+  arg.AddCallBack(func() {
+    if !filepath.IsAbs(config) {
+      config = filepath.Join(exe.Exe.AbsDir, config)
+    }
+
+    configs.SetConfigurator(&configs.JsonConfig{ReadFile: config})
+    configs.Read()
+  })
+}
