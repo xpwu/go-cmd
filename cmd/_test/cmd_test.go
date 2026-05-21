698f4597266e368c20291b297d5eedbbebd732b3
xpwu <diodexp@foxmail.com>
2026-05-21 18:13:31 +0800

    add cmd tips and modify test case

--- cmd/_test/cmd_test.go
+++ cmd/_test/cmd_test.go
@@ -1,13 +1,20 @@
 package _test
 
 import (
-  "github.com/xpwu/go-cmd/cmd"
-  _ "github.com/xpwu/go-cmd/cmd/interactive"
-  _ "github.com/xpwu/go-cmd/cmd/printconf"
-  _ "github.com/xpwu/go-cmd/cmd/validconf"
-  "testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/xpwu/go-cmd/cmd"
+	_ "github.com/xpwu/go-cmd/cmd/interactive"
+	_ "github.com/xpwu/go-cmd/cmd/printconf"
+	_ "github.com/xpwu/go-cmd/cmd/validconf"
+	"strings"
+	"testing"
 )
 
 func TestRun(t *testing.T) {
-  cmd.Run()
+	var builder strings.Builder
+	cmd.SetUsageOutput(&builder)
+	cmd.Run()
+	a := assert.New(t)
+	a.Equal("Error: Did NOT Register 'run'(DefaultCmdName) command\n",
+		builder.String())
 }
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,61 +2,67 @@ package cmd
 
 import (
 	"fmt"
-  "github.com/xpwu/go-cmd/arg"
-  "io"
-  "os"
-  "sort"
+	"github.com/xpwu/go-cmd/arg"
+	"io"
+	"os"
+	"sort"
 )
 
 type Cmd func(args *arg.Arg)
 
 var usageOutput io.Writer = os.Stderr
 
-func format(len, maxLen int) string {
-  format := "        %s"
-  for i := maxLen - len + 2; i > 0; i-- {
-    format += " "
-  }
-  format += ": %s\n"
+func SetUsageOutput(output io.Writer) {
+	usageOutput = output
+}
 
-  return format
+func format(len, maxLen int) string {
+	format := "        %s"
+	for i := maxLen - len + 2; i > 0; i-- {
+		format += " "
+	}
+	format += ": %s\n"
+
+	return format
 }
 
 func usage(args *arg.Arg) {
-  _,_ = fmt.Fprintf(usageOutput,
-    "\nUsage:\n\n        %s <command> [arguments]\n\nThe commands are: (the default command is %s) \n\n",
-    os.Args[0], DefaultCmdName)
+	_, _ = fmt.Fprintf(usageOutput,
+		"\nUsage:\n\n        %s <command> [arguments]\n\nThe commands are: (the default command is %s) \n\n",
+		os.Args[0], DefaultCmdName)
 
-  maxLen := 0
-  keys := make([]string, 0, len(helps))
-  for k,_ := range helps {
-    if len(k) > maxLen {
-      maxLen = len(k)
-    }
-    keys = append(keys, k)
-  }
+	maxLen := 0
+	keys := make([]string, 0, len(helps))
+	for k, _ := range helps {
+		if len(k) > maxLen {
+			maxLen = len(k)
+		}
+		keys = append(keys, k)
+	}
 
-  sort.Slice(keys, func(i, j int) bool {
-    return keys[i] < keys[j]
-  })
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
 
-  for _,k := range keys {
-    _,_ = fmt.Fprintf(usageOutput, format(len(k), maxLen), k, helps[k])
-  }
+	for _, k := range keys {
+		_, _ = fmt.Fprintf(usageOutput, format(len(k), maxLen), k, helps[k])
+	}
 
-  _,_ = fmt.Fprintf(usageOutput,
-    "\nUse \"%s <command> -h\" for more information about the command.\n\n", os.Args[0])
+	_, _ = fmt.Fprintf(usageOutput,
+		"\nUse \"%s <command> -h\" for more information about the command.\n\n", os.Args[0])
 
 }
 
 var cmds = map[string]Cmd{
 	"-h":   usage,
 	"help": usage,
-	DefaultCmdName: func(args *arg.Arg) {},
+	DefaultCmdName: func(args *arg.Arg) {
+		_, _ = fmt.Fprintln(usageOutput, "Error: Did NOT Register 'run'(DefaultCmdName) command")
+	},
 }
 
-var helps = map[string]string {
-  DefaultCmdName: "<not implement>",
+var helps = map[string]string{
+	DefaultCmdName: "<not implement>",
 }
 
 const DefaultCmdName = "run"
@@ -79,7 +85,7 @@ func RegisterCmd(cmdName string, help string, cmd Cmd) {
 		tryName = fmt.Sprintf("%s%d", cmdName, i)
 	}
 	cmds[tryName] = cmd
-  helps[tryName] = help
+	helps[tryName] = help
 }
 
 func RegisterCmdNoArgs(cmdName string, help string, cmd func()) {
@@ -91,25 +97,24 @@ func RegisterCmdNoArgs(cmdName string, help string, cmd func()) {
 
 func Run() {
 	args := os.Args[1:]
-  if len(args) == 0 {
-    if cmd, ok := cmds[DefaultCmdName]; ok {
-      cmd(arg.NewArg(os.Args[0], args))
-    }
-    return
-  }
+	if len(args) == 0 {
+		if cmd, ok := cmds[DefaultCmdName]; ok {
+			cmd(arg.NewArg(os.Args[0], args))
+		}
+		return
+	}
 
 	tryCmd := args[0]
 
-  if cmd, ok := cmds[tryCmd]; ok {
-    cmd(arg.NewArg(os.Args[0] + " " + tryCmd + " ", args[1:]))
-    return
-  }
+	if cmd, ok := cmds[tryCmd]; ok {
+		cmd(arg.NewArg(os.Args[0]+" "+tryCmd+" ", args[1:]))
+		return
+	}
 
-  // default
-  cmd, ok := cmds[DefaultCmdName]
-  if !ok {
-    return
-  }
-  cmd(arg.NewArg(args[0], args))
+	// default
+	cmd, ok := cmds[DefaultCmdName]
+	if !ok {
+		return
+	}
+	cmd(arg.NewArg(args[0], args))
 }
-
